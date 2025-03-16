00afa72ec06d6be76e268f55eb4d4eb6803b80ee
MiguelScofielD <antdp408@gmail.com>
2025-03-16 17:03:19 -0400

    Deploy website - based on 120d0d288cef96bf70ebb8ad5a7b02b18c25e1e6

---
+++ assets/js/acecf23e.4b9a8481.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcgeo=self.webpackChunkcgeo||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/cgeo/blog","blogTitle":"Blog","authorsListPath":"/cgeo/blog/authors"}')}}]);
