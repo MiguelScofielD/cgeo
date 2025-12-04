9254079ccdc495c9779f920158f0fde1907042b0
MiguelScofielD <antdpmiguel@gmail.com>
2025-12-04 14:15:11 -0400

    Deploy website - based on 1b14dcd5f07f0633f57c7e517f97942c3b2aaed5

---
+++ assets/js/63b6290e.4bdcbf48.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcgeo=self.webpackChunkcgeo||[]).push([[2760],{8253:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/cgeo/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
